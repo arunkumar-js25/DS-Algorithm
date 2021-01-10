cf61ad71aa529961d552ee3b02068469a3de5ab9
Arun Kumar <53009824+arun25-coder@users.noreply.github.com>
2021-01-10 08:34:01 +0530

    Update Perumtations.py

--- Perumtations.py
+++ Perumtations.py
@@ -33,6 +33,19 @@ a = list(string)
 permute(a, 0, n - 1)
 
 
+#ANOTHER IMPLEMENTATION : RECURSIVE
+def permutations(remaining, candidate=""):
+ 
+    if len(remaining) == 0:
+        print(candidate)
+ 
+    for i in range(len(remaining)):
+        newCandidate = candidate + remaining[i]
+        newRemaining = remaining[0:i] + remaining[i+1:]
+        permutations(newRemaining, newCandidate)
+
+permutations("ABC")
+
 #ITERTOOLS
 from itertools import permutations 
 print(list(permutations(range(1, 4))))
