a0ecaef21996ff4467a09724c658f7152ba4fca5
Arun Kumar <53009824+arun25-coder@users.noreply.github.com>
2021-01-11 07:05:48 +0530

    Update Pots of Gold Game Problem using Dynamic Programming.py

--- Pots of Gold Game Problem using Dynamic Programming.py
+++ Pots of Gold Game Problem using Dynamic Programming.py
@@ -26,4 +26,38 @@ def optimal_stategy(line,i,j):
 
 
 golds = [4,6,2,3]
-print(optimal_stategy(golds,0,len(golds)-1))
+print("Maximum coins collected by player is", optimal_stategy(golds,0,len(golds)-1))
+
+
+#DYNAMIC PROGRAMMING -  to avoid running recursion over and over on computed strategy
+def calculate(T, i, j):
+    return T[i][j] if i <= j else 0
+ 
+
+    
+def optimalStrategy(coin):
+    n = len(coin)
+ 
+    if n == 1:
+        return coin[0]
+    if n == 2:
+        return max(coin[0], coin[1])
+ 
+    # create a dynamic 2D matrix to store sub-problem solutions
+    T = [[0 for x in range(n)] for y in range(n)]
+ 
+    for iteration in range(n):
+        i = 0
+        j = iteration
+        while j < n:
+            start = coin[i] + min(calculate(T, i + 2, j), calculate(T, i + 1, j - 1))
+            end = coin[j] + min(calculate(T, i + 1, j - 1), calculate(T, i, j - 2))
+            T[i][j] = max(start, end)
+            i = i + 1
+            j = j + 1
+ 
+    return T[0][n - 1]
+ 
+# pots of gold arranged in a line
+coin = [4, 6, 2, 3]
+print("Maximum coins collected by player is", optimalStrategy(coin))
